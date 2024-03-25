e30d6a6807d8e5efb88473674e482e6cf4037111
stevebstorm <steve.lorent@gmail.Com>
2024-03-25 14:31:42 +0100

    InteractionComposant

---
+++ DemoMAUI/Components/Pages/Demo2.razor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoMAUI.Components.Pages
+{
+    public partial class Demo2
+    {
+        public string ReponseDuPti { get; set; }
+
+        public void RecevoirReponse(string r)
+        {
+            ReponseDuPti = r;
+        }
+    }
+}
---
+++ DemoMAUI/Components/Pages/PetitComposant.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoMAUI.Components.Pages
+{
+    public partial class PetitComposant
+    {
+        [Parameter]
+        public string ValeurDeDemo2 { get; set; }
+
+        [Parameter]
+        public EventCallback<string> Reponse { get; set; }
+
+        public void RepondADemo2()
+        {
+            Reponse.InvokeAsync(
+                "La réponse fournie par le pti");
+        }
+    }
+}
